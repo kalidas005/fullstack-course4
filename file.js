1e65c9e95578e1dbf4ce22692b1301984b04da7d
KALIDAS <66297639+kalidas005@users.noreply.github.com>
2020-06-29 23:14:39 -0700

    module 4 assignment

---
+++ file.js
@@ -0,0 +1,110 @@
+(function (window) {
+  var yaakovgreeter={};
+  yaakovgreeter.name="yaakov";
+  var greeting=" Hello ";
+  yaakovgreeter.sayHello= function () {
+	console.log(greeting +yaakovgreeter.name);
+  }
+
+  window.yaakovgreeter=yaakovgreeter;
+
+})(window);
+(function (window) {
+var johngreeter={};
+johngreeter.name="John";
+var greeting=" Good Bye ";
+johngreeter.saygoodbye=function () {
+	console.log(greeting +johngreeter.name);
+}  
+
+window.johngreeter=johngreeter;
+
+})(window);
+(function (window) {
+var jengreeter={};
+jengreeter.name="Jen";
+var greeting=" Good Bye ";
+jengreeter.saygoodbye=function () {
+	console.log(greeting +jengreeter.name);
+}  
+
+window.jengreeter=jengreeter;
+
+})(window);
+(function (window) {
+var jasongreeter={};
+jasongreeter.name="Jason";
+var greeting=" Good Bye ";
+jasongreeter.saygoodbye=function () {
+	console.log(greeting +jasongreeter.name);
+}  
+
+window.jasongreeter=jasongreeter;
+
+})(window);
+(function (window) {
+  var paulgreeter={};
+  paulgreeter.name="paul";
+  var greeting=" Hello ";
+  paulgreeter.sayHello= function () {
+	console.log(greeting +paulgreeter.name);
+  }
+
+  window.paulgreeter=paulgreeter;
+
+})(window);
+(function (window) {
+  var frankgreeter={};
+  frankgreeter.name="frank";
+  var greeting=" Hello ";
+  frankgreeter.sayHello= function () {
+	console.log(greeting +frankgreeter.name);
+  }
+
+  window.frankgreeter=frankgreeter;
+
+})(window);
+(function (window) {
+  var larrygreeter={};
+  larrygreeter.name="larry";
+  var greeting=" Hello ";
+  larrygreeter.sayHello= function () {
+	console.log(greeting +larrygreeter.name);
+  }
+
+  window.larrygreeter=larrygreeter;
+
+})(window);
+(function (window) {
+  var paulagreeter={};
+  paulagreeter.name="paula";
+  var greeting=" Hello ";
+  paulagreeter.sayHello= function () {
+	console.log(greeting +paulagreeter.name);
+  }
+
+  window.paulagreeter=paulagreeter;
+
+})(window);
+(function (window) {
+  var lauragreeter={};
+  lauragreeter.name="laura";
+  var greeting=" Hello ";
+  lauragreeter.sayHello= function () {
+	console.log(greeting +lauragreeter.name);
+  }
+
+  window.lauragreeter=lauragreeter;
+
+})(window);
+(function (window) {
+var jimgreeter={};
+jimgreeter.name="Jim";
+var greeting=" Good Bye ";
+jimgreeter.saygoodbye=function () {
+	console.log(greeting +jimgreeter.name);
+}  
+
+window.jimgreeter=jimgreeter;
+
+})(window);
